2875f2306d78c39f6161184833dcfb38746559a9
artiphishle <fabio.gartenmann@protonmail.com>
2024-02-14 13:29:47 +0100

    Fix run script

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
+const path = require("path");
 const { exec } = require('child_process');
+const scriptPath = path.join(__dirname, "scripts", "starter-expo-stack.sh");
 
-exec('sh scripts/starter-expo-stack.sh', (error, stdout, stderr) => {
+exec(scriptPath, (error, stdout, stderr) => {
   if (error) return console.error(`Error: ${error.message}`);
   if (stderr) return console.error(`Error: ${stderr}`);
   console.log(stdout);
