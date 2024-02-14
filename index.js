bdd879024449d792bed80c62849fbe002ee193b5
artiphishle <fabio.gartenmann@protonmail.com>
2024-02-14 09:23:26 +0100

    v1.0.6

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const exec = require("child_process").exec;
-exec("./scripts/starter-expo-stack.sh", (a,b,c)=>{
+exec("bash ./scripts/starter-expo-stack.sh", (a,b,c)=>{
   if(a) console.log(a);
   if(b) console.log(b);
   if(c) console.log(c);
