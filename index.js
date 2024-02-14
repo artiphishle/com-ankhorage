d7697fb2f23a02f03d3f0051ec65148b50c548a2
artiphishle <fabio.gartenmann@protonmail.com>
2024-02-14 13:31:40 +0100

    Fix npx script

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require("path");
 const { exec } = require('child_process');
 const scriptPath = path.join(__dirname, "scripts", "starter-expo-stack.sh");
 
-exec(scriptPath, (error, stdout, stderr) => {
+exec(`sh ${scriptPath}`, (error, stdout, stderr) => {
   if (error) return console.error(`Error: ${error.message}`);
   if (stderr) return console.error(`Error: ${stderr}`);
   console.log(stdout);
