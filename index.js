3fb00d5d3cabffceb7b5c8cfcdc73e31b26605b5
artiphishle <fabio.gartenmann@protonmail.com>
2024-02-14 13:15:06 +0100

    Add run script

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-#!/usr/bin/env node
-
-const exec = require("child_process").exec;
-exec("ls -la");
-// exec("bash ./scripts/starter-expo-stack.sh", (a,b,c)=>{
-  // if(a) console.log(a);
-  // if(b) console.log(b);
-  // if(c) console.log(c);
-// });
+console.log("Run:","npm run init:expo");
