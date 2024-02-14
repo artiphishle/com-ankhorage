aaa34b67128fec71896e006827019def6f6b0e19
artiphishle <fabio.gartenmann@protonmail.com>
2024-02-14 09:27:17 +0100

    v1.0.8

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
 const exec = require("child_process").exec;
-exec("bash ./scripts/starter-expo-stack.sh", (a,b,c)=>{
-  if(a) console.log(a);
-  if(b) console.log(b);
-  if(c) console.log(c);
-});
+exec("ls -la");
+// exec("bash ./scripts/starter-expo-stack.sh", (a,b,c)=>{
+  // if(a) console.log(a);
+  // if(b) console.log(b);
+  // if(c) console.log(c);
+// });
