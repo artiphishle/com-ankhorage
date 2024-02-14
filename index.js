2302cff8d9502103176b9dcfc17b63a11890dc3b
artiphishle <fabio.gartenmann@protonmail.com>
2024-02-14 13:25:19 +0100

    Fix start script

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Run:","npm run init:expo");
+#!/usr/bin/env node
+const { exec } = require('child_process');
+
+exec('sh scripts/starter-expo-stack.sh', (error, stdout, stderr) => {
+  if (error) return console.error(`Error: ${error.message}`);
+  if (stderr) return console.error(`Error: ${stderr}`);
+  console.log(stdout);
+});
